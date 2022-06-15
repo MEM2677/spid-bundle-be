e360fb40312c556d643a2a68e9cfea548ea3bef3
Matteo Emanuele Minnai <m.minnai@entando.com>
2022-06-15 14:54:26 +0200

    Initial version of spid generated by generator-jhipster@7.2.0 with blueprints: generator-jhipster-entando@7.0.1

---
+++ src/main/java/com/entando/spid/client/OAuth2InterceptedFeignConfiguration.java
@@ -0,0 +1,13 @@
+package com.entando.spid.client;
+
+import com.entando.spid.security.oauth2.AuthorizationHeaderUtil;
+import feign.RequestInterceptor;
+import org.springframework.context.annotation.Bean;
+
+public class OAuth2InterceptedFeignConfiguration {
+
+    @Bean(name = "oauth2RequestInterceptor")
+    public RequestInterceptor getOAuth2RequestInterceptor(AuthorizationHeaderUtil authorizationHeaderUtil) {
+        return new TokenRelayRequestInterceptor(authorizationHeaderUtil);
+    }
+}
